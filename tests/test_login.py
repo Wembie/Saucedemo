05cedddd5cde37853daca29c23435197a8ed2157
Juan <jeacosta@enerbit.co>
2024-10-17 11:06:59 -0500

    Change one page for all of them

--- tests/test_login.py
+++ tests/test_login.py
@@ -6,6 +6,7 @@ from selenium.webdriver.support import expected_conditions as EC
 import unittest
 from logging import Logger, basicConfig, INFO
 
+# Configuración del logger
 basicConfig(level=INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 logger = Logger("SauceDemoLogger")
 
@@ -21,6 +22,15 @@ class SauceDemoTests(unittest.TestCase):
     ]
     password = "secret_sauce"
     screenshots_dir = "screenshots"
+
+    # Mapeo de páginas a sus mensajes de error
+    pages_to_test = {
+        "https://www.saucedemo.com/cart.html": "Epic sadface: You can only access '/cart.html' when you are logged in.",
+        "https://www.saucedemo.com/inventory.html": "Epic sadface: You can only access '/inventory.html' when you are logged in.",
+        "https://www.saucedemo.com/checkout-step-one.html": "Epic sadface: You can only access '/checkout-step-one.html' when you are logged in.",
+        "https://www.saucedemo.com/checkout-step-two.html": "Epic sadface: You can only access '/checkout-step-two.html' when you are logged in.",
+        "https://www.saucedemo.com/checkout-complete.html": "Epic sadface: You can only access '/checkout-complete.html' when you are logged in.",
+    }
 
     @classmethod
     def setUpClass(cls):
@@ -84,30 +94,30 @@ class SauceDemoTests(unittest.TestCase):
                     f"Screenshot for user {user} failure saved at {screenshot_path}"
                 )
 
-    def test_access_inventory_without_login(self):
-        self.driver.get("https://www.saucedemo.com/inventory.html")
-
-        try:
-            error_message = self.wait.until(
-                EC.visibility_of_element_located(
-                    (By.CLASS_NAME, "error-message-container")
-                )
-            ).text
-            self.assertEqual(
-                error_message,
-                "Epic sadface: You can only access '/inventory.html' when you are logged in.",
-            )
-            logger.info("Access to inventory page without login correctly blocked.")
-
-        except Exception as e:
-            logger.error(
-                "Failed to verify error message for accessing inventory without login."
-            )
-            screenshot_path = os.path.join(
-                self.screenshots_dir, "access_inventory_without_login_failure.png"
-            )
-            self.driver.save_screenshot(screenshot_path)
-            logger.error(f"Screenshot saved at {screenshot_path}")
+    def test_access_pages_without_login(self):
+        for page, expected_error in self.pages_to_test.items():
+            with self.subTest(page=page):
+                self.driver.get(page)
+                try:
+                    error_message = self.wait.until(
+                        EC.visibility_of_element_located(
+                            (By.CLASS_NAME, "error-message-container")
+                        )
+                    ).text
+                    self.assertEqual(error_message, expected_error)
+                    logger.info(
+                        f"Access to page '{page}' correctly blocked without login."
+                    )
+                except Exception as e:
+                    logger.error(
+                        f"Failed to verify error message for accessing '{page}'."
+                    )
+                    screenshot_path = os.path.join(
+                        self.screenshots_dir,
+                        f"access_{page.split('/')[-1]}_without_login_failure.png",
+                    )
+                    self.driver.save_screenshot(screenshot_path)
+                    logger.error(f"Screenshot saved at {screenshot_path}")
 
     def tearDown(self):
         self.driver.quit()
